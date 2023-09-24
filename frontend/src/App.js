6f56dce9514b7adefdf651fb4adcfa94b7b221df
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2023-09-24 15:41:56 +0200

    pushing the new front end folder after problem with git pushing the orignial one

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const expirationTime = localStorage.getItem('expirationTime');
+    if (expirationTime) {
+      const currentTime = new Date().getTime();
+    }
+  }, [dispatch]);
+
+  return (
+    <>
+      <ToastContainer />
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
